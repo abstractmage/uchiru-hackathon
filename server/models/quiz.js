daf6e7e570e860dd1b8a5d3979edf754bb2dff25
Leyla Velieva <lisana5632@gmail.com>
2020-12-25 15:52:03 +0300

    feat (quiz.js)  Добавила схему

--- server/models/quiz.js
+++ server/models/quiz.js
@@ -2,6 +2,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const quizData = new Schema({});
+const questionSchema = new Schema({
+  img: {data: Buffer, required: false, contentType: String},
+  title: { type: String, required: true, max: 200},
+  answers: {type: Array, required: true },
+  rightAnswer: {type: Number, required: true },
+  timeLimit: { type: Number, required: true } ,
+});
 
-module.exports = mongoose.model("Quiz", quizData);
+const quizData = new Schema({
+  pin: { type: Number, required: true },
+  img: {data: Buffer, required: false, contentType: String},
+  title: { type: String, required: true, max: 200},
+  questions: [questionSchema],
+});
+
+const Quiz = mongoose.model("Quiz", quizData);
+
+module.exports = Quiz;
