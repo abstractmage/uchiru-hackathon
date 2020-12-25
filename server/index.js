93e0c5f5586a145423c022a3ddeac31e5cae1c8b
Leyla Velieva <lisana5632@gmail.com>
2020-12-25 11:41:58 +0300

    feat (index.js)  Добавила заглушку данных

--- server/index.js
+++ server/index.js
@@ -3,8 +3,60 @@ const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 
+
+const quizzes = [
+  {
+    pin: 1111,
+    title: 'Quiz 1',
+    questions: [
+      {
+        title: 'Question 1',
+        answers: ['answer 1', 'answer 2', 'answer 2'],
+        rightAnswer: 1,
+      },
+      {
+        title: 'Question 2',
+        answers: ['answer 1', 'answer 2', 'answer 2'],
+        rightAnswer: 0,
+      }
+    ],
+  },
+  {
+    pin: 2222,
+    title: 'Quiz 2',
+    questions: [
+      {
+        title: 'Question 1',
+        answers: ['answer 1', 'answer 2', 'answer 2'],
+        rightAnswer: 1,
+      },
+      {
+        title: 'Question 2',
+        answers: ['answer 1', 'answer 2', 'answer 2'],
+        rightAnswer: 0,
+      }
+    ],
+  },
+  {
+    pin: 3333,
+    title: 'Quiz 3',
+    questions: [
+      {
+        title: 'Question 1',
+        answers: ['answer 1', 'answer 2', 'answer 2'],
+        rightAnswer: 1,
+      },
+      {
+        title: 'Question 2',
+        answers: ['answer 1', 'answer 2', 'answer 2'],
+        rightAnswer: 0,
+      }
+    ],
+  },
+];
+
 app.get('/teacher', (req, res) => {
-  res.send('all quizzes');
+  res.send(quizzes);
 });
 
 app.get('/pupil',  (req, res) => {
