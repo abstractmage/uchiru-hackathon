cf1aa8165ff472ace3f1f69fc6e6d3dea11983df
Leyla Velieva <lisana5632@gmail.com>
2021-01-20 16:29:29 +0300

    feat (App/index) Добавила BrowserRouter

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { observer, useLocalObservable } from 'mobx-react';
-import { Route, Switch, useHistory } from 'react-router-dom';
+import { Route, Switch, useHistory, BrowserRouter } from 'react-router-dom';
 import { useBodyClassName } from '~/shared/hooks/use-body-classname';
 import style from './index.module.scss';
 import { AppStore } from './store';
@@ -36,59 +36,61 @@ export const App = observer(function App() {
   useBodyClassName();
 
   return (
-    <div className={style.main}>
-      {store.loaded && (
-        <Switch>
-          <Route path="/teacher/quizzes" exact>
-            <QuizzesPage
-              locked={store.disabled}
-              quizzes={store.teacherAppData}
-              onCreateClick={store.handleQuizzesCreateClick}
-              onStartClick={store.handleQuizzesStartClick}
-              onChangeClick={store.handleQuizzesChangeClick}
-            />
-          </Route>
-          <Route path="/teacher/quizzes/add" exact>
-            <QuizCreatingPage
-              disabled={store.disabled}
-              onSaveClick={store.handleQuizSave}
-              onBackClick={store.handleBackClick}
-            />
-          </Route>
-          <Route path="/teacher/quizzes/:id" exact>
-            {({ match }) => {
-              const pin = Number(match?.params.id);
+    <BrowserRouter basename="/kahoot">
+      <div className={style.main}>
+        {store.loaded && (
+          <Switch>
+            <Route path="/teacher/quizzes" exact>
+              <QuizzesPage
+                locked={store.disabled}
+                quizzes={store.teacherAppData}
+                onCreateClick={store.handleQuizzesCreateClick}
+                onStartClick={store.handleQuizzesStartClick}
+                onChangeClick={store.handleQuizzesChangeClick}
+              />
+            </Route>
+            <Route path="/teacher/quizzes/add" exact>
+              <QuizCreatingPage
+                disabled={store.disabled}
+                onSaveClick={store.handleQuizSave}
+                onBackClick={store.handleBackClick}
+              />
+            </Route>
+            <Route path="/teacher/quizzes/:id" exact>
+              {({ match }) => {
+                const pin = Number(match?.params.id);
 
-              if (pin === null || pin === undefined) return null;
-              const quiz = store.quizzes.find((q) => q.pin === pin)!;
-              const items = store.getQuizCreatingPageItems(pin)!;
+                if (pin === null || pin === undefined) return null;
+                const quiz = store.quizzes.find((q) => q.pin === pin)!;
+                const items = store.getQuizCreatingPageItems(pin)!;
 
-              return (
-                <QuizCreatingPage
-                  quiz={{ id: quiz._id, pin: quiz.pin, name: quiz.title, items }}
-                  disabled={store.disabled}
-                  onSaveClick={store.handleQuizSave}
-                  onBackClick={store.handleBackClick}
-                />
-              );
-            }}
-          </Route>
-          <Route path="/teacher/play/:pin" exact>
-            {({ match }) => {
-              const pin = Number(match?.params.pin);
+                return (
+                  <QuizCreatingPage
+                    quiz={{ id: quiz._id, pin: quiz.pin, name: quiz.title, items }}
+                    disabled={store.disabled}
+                    onSaveClick={store.handleQuizSave}
+                    onBackClick={store.handleBackClick}
+                  />
+                );
+              }}
+            </Route>
+            <Route path="/teacher/play/:pin" exact>
+              {({ match }) => {
+                const pin = Number(match?.params.pin);
 
-              if (pin === null || pin === undefined) return null;
+                if (pin === null || pin === undefined) return null;
 
-              return <QuizControlPage quiz={store.getQuizControlPageData(pin)!} />;
-            }}
-          </Route>
-          <Route path="/pupil/play" exact>
-            <QuizPlayPage />
-          </Route>
-          <Route>Error</Route>
-        </Switch>
-      )}
-      <Preloader shown={store.preloader.shown} visibility={store.preloader.visibility} />
-    </div>
+                return <QuizControlPage quiz={store.getQuizControlPageData(pin)!} />;
+              }}
+            </Route>
+            <Route path="/pupil/play" exact>
+              <QuizPlayPage />
+            </Route>
+            <Route>Error</Route>
+          </Switch>
+        )}
+        <Preloader shown={store.preloader.shown} visibility={store.preloader.visibility} />
+      </div>
+    </BrowserRouter>
   );
 });
